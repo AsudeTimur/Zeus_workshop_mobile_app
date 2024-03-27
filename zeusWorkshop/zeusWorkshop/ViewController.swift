304ee781ce524e904f52aa9539c476b7c4d022e8
Asude Timur <aasudetimur@gmail.com>
2024-03-27 16:25:39 +0300

    create navigation to main screen

--- zeusWorkshop/zeusWorkshop/ViewController.swift
+++ zeusWorkshop/zeusWorkshop/ViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var epostaText: UITextField!
+    
+    @IBOutlet weak var sifreText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func girisYapButon(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    @IBAction func kaydolButon(_ sender: Any) {
+    }
+    
+    @IBAction func sifreUnuttumButon(_ sender: Any) {
+    }
 }
 

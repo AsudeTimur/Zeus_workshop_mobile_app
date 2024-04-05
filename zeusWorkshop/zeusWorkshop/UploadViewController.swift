92a238ee5a806cbf5e3012755165ed6770a649b9
Asude Timur <aasudetimur@gmail.com>
2024-04-05 18:44:10 +0300

    add a component to select image

--- zeusWorkshop/zeusWorkshop/UploadViewController.swift
+++ zeusWorkshop/zeusWorkshop/UploadViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var wsImageView: UIImageView!
+    @IBOutlet weak var wsAdi: UITextField!
+    @IBOutlet weak var wsTarih: UITextField!
+    @IBOutlet weak var wsKonum: UITextField!
+    @IBOutlet weak var wsDetay: UITextField!
+    @IBOutlet weak var paylasButon: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        wsImageView.isUserInteractionEnabled = true
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+                wsImageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
+    @objc func chooseImage(){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .photoLibrary
+            present(pickerController, animated: true, completion: nil)
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            wsImageView.image = info[.originalImage] as? UIImage
+            self.dismiss(animated: true, completion: nil)
+        }
+    
+    @IBAction func paylasButonClicked(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 

2bbcc38c9991903672f6940abf21ecb896a75575
Asude Timur <asudetimu@gmail.com>
2024-06-08 14:22:18 +0300

    add validation for mandatory fields in UploadViewController

--- zeusWorkshop/zeusWorkshop/UploadViewController.swift
+++ zeusWorkshop/zeusWorkshop/UploadViewController.swift
@@ -56,54 +56,69 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     //verileri firebase'e kaydetme
     @IBAction func paylasButonClicked(_ sender: Any) {
-        let storage = Storage.storage()
-                let storageReference = storage.reference()
-                
-                let mediaFolder = storageReference.child("media")
-                
-                if let data = wsImageView.image?.jpegData(compressionQuality: 0.5) {
-                    let uuid = UUID().uuidString // Her kullanıldığında unique id'yi stringe çevirir
-                    let imageReference = mediaFolder.child("\(uuid).jpeg")
-                    imageReference.putData(data, metadata: nil) { (metadata, error) in
-                        if error != nil {
-                            self.makeAlert(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata")
-                        } else {
-                            imageReference.downloadURL() { (url, error) in
-                                if error == nil {
-                                    let imageUrl = url?.absoluteString
-                                    
-                                    // Database
-                                    let firestoreDatabase = Firestore.firestore()
-                                    // Firebase db'leri okumak, yazmak, değişikleri dinlemek vb.
-                                    var firestorReference: DocumentReference? = nil
-                                    let firestoreWorkshop = [
-                                        "imageURL": imageUrl!,
-                                        "wsEgitmen": self.wsEgitmen.text!,
-                                        "wsAdi": self.wsAdi.text!,
-                                        "wsUcret": self.wsUcret.text!,
-                                        "wsAciklama": self.wsAciklama.text!,
-                                        "wsAdres": self.wsAdres.text!,
-                                        "wsSehir": self.wsSehir.text!,
-                                        "wsKategori": self.wsKategori.text!,
-                                        "wsTarihSaat": self.wsTarihSaat.text!
-                                    ] as [String: Any]
-                                    
-                                    firestorReference = firestoreDatabase.collection("Workshops").addDocument(data: firestoreWorkshop) { (error) in
-                                        if error != nil {
-                                            self.makeAlert(titleInput: "Hata!", messageInput: error?.localizedDescription ?? "Hata")
-                                        } else {
-                                            self.clearTextFields()
+        if validateFields() {
+                    let storage = Storage.storage()
+                    let storageReference = storage.reference()
+                    let mediaFolder = storageReference.child("media")
+                    
+                    if let data = wsImageView.image?.jpegData(compressionQuality: 0.5) {
+                        let uuid = UUID().uuidString
+                        let imageReference = mediaFolder.child("\(uuid).jpeg")
+                        imageReference.putData(data, metadata: nil) { (metadata, error) in
+                            if error != nil {
+                                self.makeAlert(titleInput: "Hata", messageInput: error?.localizedDescription ?? "Hata")
+                            } else {
+                                imageReference.downloadURL() { (url, error) in
+                                    if error == nil {
+                                        let imageUrl = url?.absoluteString
+                                        let firestoreDatabase = Firestore.firestore()
+                                        let firestoreWorkshop = [
+                                            "imageURL": imageUrl!,
+                                            "wsEgitmen": self.wsEgitmen.text!,
+                                            "wsAdi": self.wsAdi.text!,
+                                            "wsUcret": self.wsUcret.text!,
+                                            "wsAciklama": self.wsAciklama.text!,
+                                            "wsAdres": self.wsAdres.text!,
+                                            "wsSehir": self.wsSehir.text!,
+                                            "wsKategori": self.wsKategori.text!,
+                                            "wsTarihSaat": self.wsTarihSaat.text!
+                                        ] as [String: Any]
+                                        
+                                        firestoreDatabase.collection("Workshops").addDocument(data: firestoreWorkshop) { (error) in
+                                            if error != nil {
+                                                self.makeAlert(titleInput: "Hata!", messageInput: error?.localizedDescription ?? "Hata")
+                                            } else {
+                                                self.clearTextFields()
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
+                } else {
+                    self.makeAlert(titleInput: "Hata", messageInput: "Lütfen tüm alanları doldurun.")
                 }
             }
-            
+
+            func validateFields() -> Bool {
+                // Check if all text fields are filled
+                if wsAdi.text?.isEmpty == true ||
+                    wsAciklama.text?.isEmpty == true ||
+                    wsEgitmen.text?.isEmpty == true ||
+                    wsAdres.text?.isEmpty == true ||
+                    wsSehir.text?.isEmpty == true ||
+                    wsUcret.text?.isEmpty == true ||
+                    wsKategori.text?.isEmpty == true ||
+                    wsTarihSaat.text?.isEmpty == true ||
+                    wsImageView.image == UIImage(named: "tap and select.png") {
+                    return false
+                }
+                return true
+            }
+
             func clearTextFields() {
-                self.wsImageView.image = UIImage(named: "placeholder.png") // or any placeholder image
+                self.wsImageView.image = UIImage(named: "tap and select.png")
                 self.wsAdi.text = ""
                 self.wsAciklama.text = ""
                 self.wsEgitmen.text = ""
@@ -113,16 +128,15 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                 self.wsKategori.text = ""
                 self.wsTarihSaat.text = ""
             }
-            
-            // UITextFieldDelegate yöntemi - karakter sınırı
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-            guard let currentText = textField.text else { return true }
-            let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
-            
-            // Sadece wsAciklama ve wsAdres alanları için sınır koy
-            if textField == wsAciklama || textField == wsAdres {
-                return newText.count <= 130
+
+            func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+                guard let currentText = textField.text else { return true }
+                let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+                
+                // Set character limit for wsAciklama and wsAdres fields
+                if textField == wsAciklama || textField == wsAdres {
+                    return newText.count <= 130
+                }
+                return true
             }
-            return true
         }
-}
